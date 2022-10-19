bd75a8cbcf7293e4ba3b308eea7c549845608cda
shakhzodbekpulatbekov <73734816+shakhzodbekpulatbekov@users.noreply.github.com>
2022-10-20 00:18:57 +0500

    update

--- src/main/java/com/example/kpi/Main.java
+++ src/main/java/com/example/kpi/Main.java
@@ -139,7 +139,7 @@ public class Main extends TelegramLongPollingBot {
                 }else if (text.equals("Excel fayl olish")){
                     WriteToExcel writeToExcel = new WriteToExcel(timeRepository);
                     writeToExcel.writeToFile();
-                    String path= "KPI/XodimlarRo'yxati.xls";
+                    String path= "target/classes/XodimlarRo'yxati.xls";
                     sendDocument(chat_id, new File(path), "Xodimlar ro'yxati");
                 }
 
