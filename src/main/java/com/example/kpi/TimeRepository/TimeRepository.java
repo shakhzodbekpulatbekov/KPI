bce53691fd71cd51dcc373be12d1c149b83582cd
shakhzodbekpulatbekov <73734816+shakhzodbekpulatbekov@users.noreply.github.com>
2022-10-27 01:12:08 +0500

    update

--- src/main/java/com/example/kpi/TimeRepository/TimeRepository.java
+++ src/main/java/com/example/kpi/TimeRepository/TimeRepository.java
@@ -4,7 +4,6 @@ import com.example.kpi.TimeEntity.TimeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface TimeRepository extends JpaRepository<TimeEntity,Integer> {
     List<TimeEntity> findByDate(String date);
@@ -12,6 +11,6 @@ public interface TimeRepository extends JpaRepository<TimeEntity,Integer> {
     void deleteByUserName(String name);
     List<TimeEntity>findByUserName(String name);
 
-    Optional<TimeEntity> findByUserName(String name);
+    List<TimeEntity> findByUserName(String name);
 
 }
