d7dd0d0e4d576c77c34407ebb810bd787a56d2ba
shakhzodbekpulatbekov <73734816+shakhzodbekpulatbekov@users.noreply.github.com>
2022-11-03 14:39:57 +0500

    test

--- src/main/java/com/example/kpi/Excel/WriteToExcel.java
+++ src/main/java/com/example/kpi/Excel/WriteToExcel.java
@@ -3,10 +3,13 @@ package com.example.kpi.Excel;
 import com.example.kpi.KpiRepository.KpiRepository;
 import com.example.kpi.TimeEntity.TimeEntity;
 import com.example.kpi.TimeRepository.TimeRepository;
+import org.apache.poi.hssf.usermodel.HSSFCell;
 import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.hssf.util.HSSFColor;
 import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.FillPatternType;
 
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -34,10 +37,21 @@ public class WriteToExcel {
                 row.createCell(1).setCellValue("SOAT");
                 row.createCell(2).setCellValue("ISM");
 
+                CellStyle style=xssfWorkbook.createCellStyle();
+//                style.setFillBackgroundColor(IndexedColors.RED1.getIndex());
+                style.setFillForegroundColor(HSSFColor.HSSFColorPredefined.LIGHT_ORANGE.getIndex());
+                style.setFillPattern(FillPatternType.FINE_DOTS);
+
 
                 for (int i = 0; i < users.size(); i++) {
                     row=xssfSheet.createRow(i+1);
                     row.createCell(0).setCellValue(users.get(i).getDate());
+
+                    HSSFCell cell=row.createCell(0);
+                    cell.setCellValue(users.get(i).getTime());
+                    if (!(users.get(i).getTime().startsWith("08"))){
+                        cell.setCellStyle(style);
+                    }
                     row.createCell(1).setCellValue(users.get(i).getTime());
                     row.createCell(2).setCellValue(users.get(i).getUserName());
                 }
